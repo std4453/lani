4e0f91deb8736200368d3e1e6b4fc96dbc67e2c5
std4453 <me@std4453.com>
2022-01-17 01:09:11 +0800

    feat(cli): 使用 github API 触发 workflow 并优化 devops 体验

--- tools/cli/src/commands/devops/index.ts
+++ tools/cli/src/commands/devops/index.ts
@@ -5,7 +5,8 @@ import kleur from "kleur";
 import path from "path";
 import simpleGit, { SimpleGit } from "simple-git";
 import * as _ from "lodash";
-import inclusion from "inclusion";
+import { Octokit } from "octokit";
+import globalConfig from "../../config";
 
 const DEFAULT_WORKFLOW = "node_default.yaml";
 
@@ -13,17 +14,14 @@ export default class Devops extends Command {
   static description = "Trigger CI workflow";
 
   static flags = {
-    "no-git": Flags.boolean(),
-    strict: Flags.boolean({
-      default: false,
-    }),
-    ref: Flags.string(),
-    "dry-run": Flags.boolean(),
+    "no-auto-push": Flags.boolean(),
+    strict: Flags.boolean(),
+    "log-git": Flags.boolean(),
   };
 
   async run(): Promise<void> {
     const {
-      flags: { "no-git": noGit, strict, ref, "dry-run": dry },
+      flags: { "no-auto-push": noAutoPush, strict, "log-git": logGit },
     } = await this.parse(Devops);
 
     const project = resolveProjectConfig();
@@ -35,89 +33,106 @@ export default class Devops extends Command {
     }
 
     console.log(
-      kleur.gray(
-        `Command \"devops\" requires \"git\" and \"gh\" to be in $PATH`
-      )
+      kleur.gray(`Command \"devops\" requires \"git\" to be in $PATH`)
     );
 
     const { image, workflow = DEFAULT_WORKFLOW } = config.ci;
 
-    const args = {
-      ref,
-      image_name: image,
-      project_path: path.relative(project.monorepoRoot, project.path),
-      project_name: project.packageName,
-    };
+    const git: SimpleGit = simpleGit({
+      baseDir: project.monorepoRoot,
+    });
 
-    if (!noGit) {
-      const git: SimpleGit = simpleGit({
-        baseDir: project.monorepoRoot,
+    if (logGit) {
+      git.outputHandler((command, stdout, stderr, args) => {
+        console.log(kleur.gray(`${command} ${args.join(" ")}`));
+        stdout.pipe(process.stdout);
+        stderr.pipe(process.stderr);
       });
+    }
 
-      await git.fetch();
+    await git.fetch();
 
-      const { current, tracking, ahead, behind, detached, files } =
-        await git.status();
+    const { current, tracking, ahead, behind, detached, files } =
+      await git.status();
 
-      if (strict && files.length !== 0) {
-        console.log(kleur.red("Working tree not clean, aborting"));
-        process.exit(1);
-      }
+    if (strict && files.length !== 0) {
+      console.log(kleur.red("Working tree not clean, aborting"));
+      process.exit(1);
+    }
 
-      if (detached) {
-        console.log(kleur.red("Repo in detached HEAD mode, aborting"));
-        process.exit(1);
-      }
+    if (detached) {
+      console.log(kleur.red("Repo in detached HEAD mode, aborting"));
+      process.exit(1);
+    }
 
-      if (!tracking) {
-        console.log(
-          kleur.red("Current branch has no tracking branch, aborting")
-        );
-        process.exit(1);
-      }
+    if (!tracking) {
+      console.log(kleur.red("Current branch has no tracking branch, aborting"));
+      process.exit(1);
+    }
 
-      // tracking would be like origin/master, we won't need origin/ here
-      args.ref = tracking.substring(tracking.lastIndexOf("/") + 1);
+    // tracking would be like origin/master, we won't need origin/ here
+    const ref = tracking.substring(tracking.lastIndexOf("/") + 1);
 
-      if (ahead !== 0 || behind !== 0) {
+    if (ahead !== 0 || behind !== 0) {
+      if (noAutoPush) {
         console.log(
           kleur.yellow(
             `Branch '${current}' is ahead ${ahead} and behind ${behind} of remote '${tracking}', pipeline will use inconsistent commits`
           )
         );
-
         if (strict) {
           process.exit(1);
         }
       } else {
-        console.log(kleur.green("All up-to-date, good to go!"));
+        console.log(
+          kleur.yellow(
+            `Branch '${current}' is ahead ${ahead} and behind ${behind} of remote '${tracking}', pushing to remote...`
+          )
+        );
+        await git.push();
+        console.log(`Push ${kleur.green("success")}`);
       }
-    }
-
-    if (!args.ref) {
-      console.log(kleur.red("Ref not found, aborting"));
-      process.exit(1);
-    }
-
-    const ghArgs = [
-      "workflow",
-      "run",
-      workflow,
-      ..._.toPairs(args).flatMap(([key, value]) => ["-f", `${key}=${value}`]),
-    ];
-    if (!dry) {
-      const { execa } = await inclusion("execa");
-
-      const subprocess = execa("gh", ghArgs);
-      subprocess.stdout?.pipe(process.stdout);
-      subprocess.stderr?.pipe(process.stderr);
-      await subprocess;
-
-      console.log(
-        `Workflow triggered, visit https://github.com/std4453/lani/actions/workflows/${workflow} to see the workflow run`
-      );
     } else {
-      console.log("gh", ...ghArgs);
+      console.log(kleur.green("All up-to-date, good to go!"));
+    }
+
+    const octokit = new Octokit({ auth: globalConfig.githubPAT });
+
+    await octokit.rest.actions.createWorkflowDispatch({
+      owner: globalConfig.owner,
+      repo: globalConfig.repo,
+      workflow_id: workflow,
+      ref,
+      inputs: {
+        ref,
+        image_name: image,
+        project_path: path.relative(project.monorepoRoot, project.path),
+        project_name: project.packageName,
+      },
+    });
+
+    const {
+      data: {
+        workflow_runs: [run],
+      },
+    } = await octokit.rest.actions.listWorkflowRuns({
+      owner: globalConfig.owner,
+      repo: globalConfig.repo,
+      workflow_id: workflow,
+      event: "workflow_dispatch",
+    });
+
+    if (!run) {
+      console.log(
+        `Unable to find workflow run, visit ${kleur.cyan(
+          `https://github.com/std4453/lani/actions/workflows/${workflow}`
+        )} to view details`
+      );
+      process.exit(1);
+    } else {
+      console.log(
+        `Workflow triggered, visit ${kleur.cyan(run.html_url)} to view details`
+      );
     }
   }
 }
