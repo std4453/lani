cbab3f65611c105618820a220dd85bea8d0af44d
std4453 <me@std4453.com>
2022-01-17 00:02:36 +0800

    feat(bilibili-bangumi-cc): 使用 ssid，走 API 代理

--- interfaces/api/src/services/bilibiliBangumiCC.ts
+++ interfaces/api/src/services/bilibiliBangumiCC.ts
@@ -3,8 +3,28 @@ import { buildServiceConfig } from "@/utils";
 import * as t from "io-ts";
 
 export const tDownloadSRTRequest = t.type({
-  epid: t.string,
-  language: t.string,
+  /**
+   * 不应包括前缀 ss
+   */
+  ssid: t.string,
+  /**
+   * 从 1 开始的剧集编号
+   */
+  index: t.number,
+  /**
+   * 默认为 zh-Hant
+   */
+  language: t.union([t.undefined, t.string]),
+  /**
+   * 针对地区使用不同的 proxy, 默认 thm
+   */
+  region: t.union([
+    t.undefined,
+    // 港澳台, Taiwan, Hong Kong and Macau
+    t.literal("thm"),
+    t.literal("mainland"),
+    t.literal("global"),
+  ]),
 });
 
 export type DownloadSRTRequest = t.TypeOf<typeof tDownloadSRTRequest>;
