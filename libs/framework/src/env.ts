34ce381e8a6f0a931869aafb7b44f5752a53ba97
std4453 <me@std4453.com>
2022-02-12 02:11:00 +0800

    feat(framework): 添加 offline 和 pre

--- libs/framework/src/env.ts
+++ libs/framework/src/env.ts
@@ -3,6 +3,10 @@ export type Env = "dev" | "offline" | "pre" | "prod";
 function getEnv(): Env {
   if (process.env.NODE_ENV === "development") {
     return "dev";
+  } else if (process.env.NODE_ENV === "offline") {
+    return "offline";
+  } else if (process.env.NODE_ENV === "staging") {
+    return "pre";
   } else {
     // TODO: more env
     return "prod";
