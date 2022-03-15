729f419a5c754df3cdba19dd500d3a1b10e49adf
std4453 <me@std4453.com>
2022-03-15 21:34:30 +0800

    feat(data-server): use lani-offline

--- apps/data-server/src/index.ts
+++ apps/data-server/src/index.ts
@@ -1,27 +1,16 @@
 import OmitArchivedPlugin from "@graphile-contrib/pg-omit-archived";
-import { dataServerService, DataServerService } from "@lani/api";
-import { buildApp, startApp } from "@lani/framework";
+import { dataServerService } from "@lani/api";
+import { App } from "@lani/framework";
 import { postgraphile } from "postgraphile";
 import ConnectionFilterPlugin from "postgraphile-plugin-connection-filter";
 import config from "./config";
 
-async function run() {
-  const app = buildApp<DataServerService>(
-    {},
-    {
-      cors: true,
-      setup(app) {
-        app.use(
-          postgraphile(config.postgresUrl, "public", {
-            ...config.postgraphile,
-            appendPlugins: [OmitArchivedPlugin, ConnectionFilterPlugin],
-          })
-        );
-      },
-    }
-  );
-
-  startApp(app, dataServerService);
-}
-
-run();
+const app = new App();
+app.setupMiddlewares();
+app.app.use(
+  postgraphile(config.postgresUrl, "public", {
+    ...config.postgraphile,
+    appendPlugins: [OmitArchivedPlugin, ConnectionFilterPlugin],
+  })
+);
+app.start(dataServerService);
--- apps/data-server/src/scripts/generate-schema.ts
+++ apps/data-server/src/scripts/generate-schema.ts
@@ -1,15 +1,19 @@
 import { createPostGraphileSchema } from "postgraphile";
 import { printSchema } from "graphql";
-import config from "../config";
+import config from "@/config";
 import { writeFile } from "fs/promises";
 import ConnectionFilterPlugin from "postgraphile-plugin-connection-filter";
 
 (async () => {
-  const schema = await createPostGraphileSchema(config.postgresUrl, "public", {
-    appendPlugins: [ConnectionFilterPlugin],
-  });
+  const schema = await createPostGraphileSchema(
+    config.postgresUrl ?? "",
+    "public",
+    {
+      appendPlugins: [ConnectionFilterPlugin],
+    }
+  );
   const schemaString = printSchema(schema);
 
-  const schemaPath = "src/generated/schema.graphql";
+  const schemaPath = "generated/schema.graphql";
   await writeFile(schemaPath, schemaString, "utf-8");
 })();
