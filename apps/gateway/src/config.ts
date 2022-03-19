bfdfe3744da852ea5c3d141c56d251fd2058e241
std4453 <me@std4453.com>
2022-03-20 05:42:01 +0800

    feat: add gateway

---
+++ apps/gateway/src/config.ts
@@ -0,0 +1,44 @@
+import { ServiceEndpointDefinition } from "@apollo/gateway";
+import { mergeConfig } from "@lani/framework";
+
+export default mergeConfig<{
+  subgraphs: ServiceEndpointDefinition[];
+}>({
+  subgraphs: [
+    {
+      name: "data",
+      url: "http://data-server.lani:8080",
+    },
+    {
+      name: "api",
+      url: "http://api-server.lani:8080",
+    },
+  ],
+})<{
+  subgraphs: ServiceEndpointDefinition[];
+}>({
+  dev: {
+    subgraphs: [
+      {
+        name: "data",
+        url: "http://data-server.lani-offline:8080/graphql",
+      },
+      {
+        name: "api",
+        url: "http://api-server.lani-offline:8080/graphql",
+      },
+    ],
+  },
+  offline: {
+    subgraphs: [
+      {
+        name: "data",
+        url: "http://data-server.lani-offline:8080/graphql",
+      },
+      {
+        name: "api",
+        url: "http://api-server.lani-offline:3000/graphql",
+      },
+    ],
+  },
+});
