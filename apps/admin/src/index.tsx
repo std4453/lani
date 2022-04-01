1405f11afd2192a2fb7535b25cf577de8486bd09
std4453 <me@std4453.com>
2022-04-01 17:01:22 +0800

    fix(admin): Remove old pages

--- apps/admin/src/index.tsx
+++ apps/admin/src/index.tsx
@@ -8,7 +8,6 @@ import { ElementType } from 'react';
 import { Link } from 'umi';
 import styles from './index.module.less';
 import laniText from '@/assets/lani-text.svg';
-import { DialogProvider } from '@/components/dialog';
 
 const pathToIcon: { [x: string]: ElementType } = {
   '/': HomeOutlined,
@@ -19,39 +18,37 @@ const pathToIcon: { [x: string]: ElementType } = {
 export default function App(props: any) {
   return (
     <ApolloProvider client={client}>
-      <DialogProvider>
-        <ProLayout
-          {...props}
-          navTheme="light"
-          headerRender={false}
-          style={{
-            height: '100vh',
-          }}
-          collapsed={false}
-          collapsedButtonRender={false}
-          menuHeaderRender={() => (
-            <div className={styles.logoContainer}>
-              <div className={clsx(styles.logoBlock)}>
-                <img src={laniText} alt="Lani" className={styles.logo} />
-              </div>
+      <ProLayout
+        {...props}
+        navTheme="light"
+        headerRender={false}
+        style={{
+          height: '100vh',
+        }}
+        collapsed={false}
+        collapsedButtonRender={false}
+        menuHeaderRender={() => (
+          <div className={styles.logoContainer}>
+            <div className={clsx(styles.logoBlock)}>
+              <img src={laniText} alt="Lani" className={styles.logo} />
             </div>
-          )}
-          menuItemRender={(item, dom) => {
-            const Icon = item.path ? pathToIcon[item.path] : undefined;
-            const icon = Icon ? <Icon /> : null;
-            return (
-              <Link to={item.path ?? '/'}>
-                {icon}
-                {dom}
-              </Link>
-            );
-          }}
-          contentStyle={{
-            margin: 0,
-            backgroundColor: '#FFFFFF',
-          }}
-        />
-      </DialogProvider>
+          </div>
+        )}
+        menuItemRender={(item, dom) => {
+          const Icon = item.path ? pathToIcon[item.path] : undefined;
+          const icon = Icon ? <Icon /> : null;
+          return (
+            <Link to={item.path ?? '/'}>
+              {icon}
+              {dom}
+            </Link>
+          );
+        }}
+        contentStyle={{
+          margin: 0,
+          backgroundColor: '#FFFFFF',
+        }}
+      />
     </ApolloProvider>
   );
 }
