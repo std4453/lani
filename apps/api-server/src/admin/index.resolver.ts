223061245bdc2b12f649fd02c49598acb5331ce4
std4453 <me@std4453.com>
2022-04-01 16:59:42 +0800

    feat(api-server): Admin-related API

---
+++ apps/api-server/src/admin/index.resolver.ts
@@ -0,0 +1,84 @@
+import {
+  SearchBangumiSeason,
+  UpdateSeasonDownloadSourcesInput,
+} from '@/admin/index.model';
+import { BangumiAPIService, ResponseGroup, SubjectType } from '@/bangumi';
+import { PrismaService } from '@/common/prisma.service';
+import { Injectable } from '@nestjs/common';
+import { Args, ID, Mutation, Query, Resolver } from '@nestjs/graphql';
+
+@Injectable()
+@Resolver()
+export class AdminResolver {
+  constructor(private prisma: PrismaService) {}
+
+  @Mutation(() => ID)
+  async updateSeasonDownloadSources(
+    @Args({
+      name: 'input',
+      type: () => UpdateSeasonDownloadSourcesInput,
+    })
+    { seasonId, sources }: UpdateSeasonDownloadSourcesInput,
+  ) {
+    await this.prisma.$transaction([
+      this.prisma.downloadSource.deleteMany({
+        where: {
+          seasonId: seasonId,
+          id: {
+            notIn: sources.filter(({ id }) => id !== 0).map(({ id }) => id),
+          },
+        },
+      }),
+      ...sources
+        .filter(({ id }) => id !== 0)
+        .map(({ id, pattern }) =>
+          this.prisma.downloadSource.update({
+            where: {
+              id: id,
+            },
+            data: {
+              pattern,
+            },
+          }),
+        ),
+      this.prisma.downloadSource.createMany({
+        data: sources
+          .filter(({ id }) => id === 0)
+          .map(({ pattern }) => ({
+            seasonId,
+            pattern,
+          })),
+      }),
+    ]);
+    return 'ok';
+  }
+
+  @Query(() => [SearchBangumiSeason])
+  async searchBangumi(
+    @Args('keywords') keywords: string,
+  ): Promise<SearchBangumiSeason[]> {
+    const results = await BangumiAPIService.getSearchSubject(
+      keywords,
+      SubjectType._2,
+      ResponseGroup.SMALL,
+      0,
+      25,
+    );
+    const ids = (results.list ?? []).map(({ id }) => `${id}`);
+    const added = await this.prisma.season.findMany({
+      where: {
+        bangumiId: {
+          in: ids,
+        },
+      },
+    });
+    return (results.list ?? []).map((item) => ({
+      id: `${item.id}`,
+      name: item.name_cn || item.name || '未命名',
+      airDate: item.air_date,
+      image: item.images?.small,
+      added:
+        added.find((season) => season.bangumiId === `${item.id}`) !== undefined,
+    }));
+  }
+}
