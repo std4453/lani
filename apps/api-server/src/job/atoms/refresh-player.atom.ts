a5717f7a7aa10546a176fbee07bd663b15a7eff4
std4453 <me@std4453.com>
2022-04-03 00:41:29 +0800

    feat(api-server): Make refresh player atom work by polling

--- apps/api-server/src/job/atoms/refresh-player.atom.ts
+++ apps/api-server/src/job/atoms/refresh-player.atom.ts
@@ -1,14 +1,22 @@
 import { PrismaService } from '@/common/prisma.service';
 import { ConfigType } from '@/config';
 import { ItemRefreshService, TvShowsService } from '@/jellyfin';
-import { AsyncAtom, StepInput } from '@/job/atoms';
+import { Atom, StepInput } from '@/job/atoms';
 import { DownloadWorkflowDefinition } from '@/job/atoms/types';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { EventEmitter2 } from '@nestjs/event-emitter';
+import { Int, Mutation } from '@nestjs/graphql';
+import { Cron } from '@nestjs/schedule';
+
+interface QueueItem {
+  jellyfinSeriesId: string;
+  index: number;
+  enqueueTime: number;
+}
 
 @Injectable()
-export class RefreshPlayerAtom extends AsyncAtom<
+export class RefreshPlayerAtom extends Atom<
   DownloadWorkflowDefinition,
   'refreshPlayer'
 > {
@@ -20,26 +28,56 @@ export class RefreshPlayerAtom extends AsyncAtom<
     super(emitter, 'refreshPlayer');
   }
 
-  async run(
-    _id: number,
-    { params: { episodeId }, steps }: StepInput<DownloadWorkflowDefinition>,
+  private queue: Record<number, QueueItem> = {};
+
+  async enqueue(
+    id: number,
+    { steps, params: { episodeId } }: StepInput<DownloadWorkflowDefinition>,
   ) {
     if (!steps.writeMetadata) {
-      throw new Error('writeMetadata step not finished');
+      this.jobFail(id, new Error('writeMetadata step not finished'));
+      return;
     }
-    const {
-      index,
-      season: { jellyfinId: jellyfinSeriesId },
-    } = await this.prisma.episode.findUnique({
-      where: { id: episodeId },
-      include: {
-        season: true,
-      },
-    });
-    if (!jellyfinSeriesId) {
-      throw new Error('cannot refresh since season has no jellyfinId');
+    try {
+      const {
+        index,
+        season: { jellyfinId: jellyfinSeriesId },
+      } = await this.prisma.episode.findUnique({
+        where: { id: episodeId },
+        include: {
+          season: true,
+        },
+      });
+      if (!jellyfinSeriesId) {
+        this.jobFail(
+          id,
+          new Error('cannot refresh since season has no jellyfinId'),
+        );
+        return;
+      }
+      const jellyfinEpisodeId = await this.getJellyfinEpisode(
+        index,
+        jellyfinSeriesId,
+      );
+      // 如果已经存在，则跳过刷新，直接获取
+      if (jellyfinEpisodeId) {
+        this.jobSuccess(id, {
+          jellyfinEpisodeId,
+        });
+      } else {
+        await ItemRefreshService.post(jellyfinSeriesId);
+        this.queue[id] = {
+          jellyfinSeriesId,
+          index,
+          enqueueTime: new Date().getTime(),
+        };
+      }
+    } catch (error) {
+      this.jobFail(id, error);
     }
-    await ItemRefreshService.post(jellyfinSeriesId);
+  }
+
+  private async getJellyfinEpisode(index: number, jellyfinSeriesId: string) {
     const { Items: episodes } = await TvShowsService.getEpisodes(
       jellyfinSeriesId,
       this.config.get('jellyfinUserId'),
@@ -48,11 +86,56 @@ export class RefreshPlayerAtom extends AsyncAtom<
       (episode) =>
         episode.IndexNumber === index && episode.ParentIndexNumber === 1,
     )?.Id;
-    if (!jellyfinEpisodeId) {
-      throw new Error('jellyfin episode not found');
+    return jellyfinEpisodeId;
+  }
+
+  private async refreshQueueItem(
+    id: number,
+    { enqueueTime, index, jellyfinSeriesId }: QueueItem,
+  ) {
+    try {
+      const jellyfinEpisodeId = await this.getJellyfinEpisode(
+        index,
+        jellyfinSeriesId,
+      );
+      if (!jellyfinEpisodeId) {
+        const now = new Date().getTime();
+        // 5分钟内没有完成就算失败
+        if (now - enqueueTime > 5 * 60 * 1000) {
+          this.jobFail(
+            id,
+            new Error('episode waiting for more than 5 minutes'),
+          );
+          return true;
+        }
+        return false;
+      }
+      this.jobSuccess(id, {
+        jellyfinEpisodeId,
+      });
+      return true;
+    } catch (error) {
+      this.jobFail(id, error);
+      return true;
     }
-    return {
-      jellyfinEpisodeId,
-    };
+  }
+
+  @Cron('*/15 * * * * *') // 每 15 秒
+  @Mutation(() => Int)
+  async refreshAllPlayerWaitingStatus() {
+    const ids = Object.keys(this.queue).map((idStr) => parseInt(idStr, 10));
+    if (ids.length === 0) {
+      return 0;
+    }
+    const changes = await Promise.all(
+      ids.map(async (id) => {
+        const changed = await this.refreshQueueItem(id, this.queue[id]);
+        if (changed) {
+          delete this.queue[id];
+        }
+        return changed;
+      }),
+    );
+    return changes.filter((change) => change).length;
   }
 }
