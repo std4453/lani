87a4eabbb4b58c82a5dd98fc901a213afa05d803
std4453 <me@std4453.com>
2022-04-06 19:42:03 +0800

    feat(admin): 稍微优化加载中样式

--- apps/admin/src/wrappers/auth.tsx
+++ apps/admin/src/wrappers/auth.tsx
@@ -1,12 +1,25 @@
 import { useAuth } from '@/index';
 import Page401 from '@/pages/401';
 import Page403 from '@/pages/403';
+import { Spin } from 'antd';
 import { ReactNode } from 'react';
 
 export default function AuthWrapper({ children }: { children?: ReactNode }) {
   const { authroized, authenticated, loading } = useAuth();
   if (loading) {
-    return <div>logging in</div>;
+    return (
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Spin size="large" />
+      </div>
+    );
   } else if (!authenticated) {
     return <Page401 />;
   } else if (!authroized) {
