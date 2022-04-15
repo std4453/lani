1fc2f87262af33fbe123ebdaea9059a415456c4a
std4453 <me@std4453.com>
2022-04-16 00:28:46 +0800

    feat(data-server): 启用connectionFilterRelations

--- apps/data-server/src/config.ts
+++ apps/data-server/src/config.ts
@@ -1,31 +1,25 @@
+import PgSimplifyInflectorPlugin from "@graphile-contrib/pg-simplify-inflector";
 import { mergeConfig } from "@lani/framework";
 import { PostGraphileOptions } from "postgraphile";
-import PgSimplifyInflectorPlugin from "@graphile-contrib/pg-simplify-inflector";
 
-export default mergeConfig({})<{
-  postgresUrl: string;
-  postgraphile: PostGraphileOptions;
-}>({
+export default mergeConfig({
+  postgresUrl:
+    "postgres://postgraphile:3KS0n*TXW4!VYYC!%24gFc25mM@data-postgresql.postgres:5432/lani",
+  postgraphile: {
+    subscriptions: true,
+    retryOnInitFail: true,
+    dynamicJson: true,
+    setofFunctionsContainNulls: false,
+    ignoreRBAC: false,
+    extendedErrors: ["errcode"],
+    appendPlugins: [PgSimplifyInflectorPlugin],
+    graphiql: false,
+    enableQueryBatching: true,
+    disableQueryLog: true,
+    legacyRelations: "omit",
+  } as PostGraphileOptions,
+})({
   dev: {
-    postgresUrl:
-      "postgres://postgres:a*qLweVSC!4yRvBNP%405VGfyR@10.43.222.73:5432/lani-offline",
-    postgraphile: {
-      subscriptions: true,
-      dynamicJson: true,
-      setofFunctionsContainNulls: true,
-      ignoreRBAC: true,
-      showErrorStack: "json",
-      extendedErrors: ["hint", "detail", "errcode"],
-      graphiql: true,
-      enhanceGraphiql: true,
-      allowExplain: true,
-      enableQueryBatching: true,
-      legacyRelations: "omit",
-      watchPg: true,
-      exportGqlSchemaPath: "./generated/schema.graphql",
-    },
-  },
-  offline: {
     postgresUrl:
       "postgres://postgres:a*qLweVSC!4yRvBNP%405VGfyR@data-postgresql.postgres:5432/lani-offline",
     postgraphile: {
@@ -41,25 +35,24 @@ export default mergeConfig({})<{
       enableQueryBatching: true,
       legacyRelations: "omit",
       watchPg: true,
-      externalUrlBase: "/api/data-server",
-    },
+      exportGqlSchemaPath: "./generated/schema.graphql",
+    } as PostGraphileOptions,
   },
-  prod: {
+  offline: {
     postgresUrl:
-      "postgres://postgraphile:3KS0n*TXW4!VYYC!%24gFc25mM@data-postgresql.postgres:5432/lani",
+      "postgres://postgres:a*qLweVSC!4yRvBNP%405VGfyR@data-postgresql.postgres:5432/lani-offline",
     postgraphile: {
       subscriptions: true,
-      retryOnInitFail: true,
       dynamicJson: true,
-      setofFunctionsContainNulls: false,
-      ignoreRBAC: false,
-      extendedErrors: ["errcode"],
-      appendPlugins: [PgSimplifyInflectorPlugin],
+      setofFunctionsContainNulls: true,
+      ignoreRBAC: true,
+      showErrorStack: "json",
+      extendedErrors: ["hint", "detail", "errcode"],
       graphiql: false,
+      allowExplain: false,
       enableQueryBatching: true,
-      disableQueryLog: true,
       legacyRelations: "omit",
-      externalUrlBase: "/api/data-server",
-    },
+      watchPg: true,
+    } as PostGraphileOptions,
   },
 });
