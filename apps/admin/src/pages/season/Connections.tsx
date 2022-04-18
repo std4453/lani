ed49750882b8d8dd5a899e0e41dfe484718f8c4e
std4453 <me@std4453.com>
2022-04-18 15:21:25 +0800

    fix(admin): 多了个逗号

--- apps/admin/src/pages/season/Connections.tsx
+++ apps/admin/src/pages/season/Connections.tsx
@@ -295,7 +295,6 @@ export default function Connections() {
                 />
               )}
             </FormDependency>
-            ,
           </Input.Group>
         </Form.Item>
       </ProForm.Group>
